f821446eaf050cedb3ae3d4e6b0e8159bb11b94b
BlackNight Shadow <65853201+BNLOS@users.noreply.github.com>
2023-04-22 02:06:12 +0300

    Update PDFDownloader.js

--- PDFDownloader.js
+++ PDFDownloader.js
@@ -1,3 +1,17 @@
+// ==UserScript==
+// @name        Twitter Media Downloader
+// @name:ja     Twitter Media Downloader
+// @name:zh-cn  Twitter 媒体下载
+// @name:zh-tw  Twitter 媒體下載
+// @description    Save Video/Photo by One-Click.
+// @description:ja ワンクリックで動画・画像を保存する。
+// @description:zh-cn 一键保存视频/图片
+// @description:zh-tw 一鍵保存視頻/圖片
+// @version     1.05
+// @author      AMANE
+// @namespace   none
+// @match       http*://*/*
+// ==/UserScript==
 // Get the PDF url
 const pdfUrl = 'https://*.pdf';
 
