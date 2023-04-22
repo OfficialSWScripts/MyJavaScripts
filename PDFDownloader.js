aa945b50a000073beeadaaa0e9901ac0cee58f5e
BlackNight Shadow <65853201+BNLOS@users.noreply.github.com>
2023-04-22 18:35:01 +0300

    Update PDFDownloader.js

--- PDFDownloader.js
+++ PDFDownloader.js
@@ -5,30 +5,3 @@
 // @author      BNLOS
 // @match       http*://*/*
 // ==/UserScript==
-// Get the PDF url
-const pdfUrl = 'https://*.pdf';
-
-// Create a button element
-const downloadButton = document.createElement('button');
-downloadButton.textContent = 'Download PDF';
-
-// Add a click event listener to the button
-downloadButton.addEventListener('click', () => {
-  // Create a hidden anchor element
-  const downloadLink = document.createElement('a');
-  downloadLink.style.display = 'none';
-  downloadLink.href = pdfUrl;
-  downloadLink.setAttribute('download', '*.pdf');
-  
-  // Append the anchor element to the document body
-  document.body.appendChild(downloadLink);
-  
-  // Trigger a click event on the anchor element
-  downloadLink.click();
-  
-  // Clean up the anchor element
-  document.body.removeChild(downloadLink);
-});
-
-// Add the button to the webpage
-document.body.appendChild(downloadButton);
