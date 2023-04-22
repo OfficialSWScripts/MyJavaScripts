e9fa10cb23b72860635915916ffdd25111a86f01
BlackNight Shadow <65853201+BNLOS@users.noreply.github.com>
2023-04-22 17:35:30 +0300

    Update PSBypass.js

--- PSBypass.js
+++ PSBypass.js
@@ -5,21 +5,22 @@
 // @description  This Script Should Bypass Transaction Verification and returns Success Code
 // @match        https://store.playstation.com/*
 // ==/UserScript==
-const xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4) {
-        if (this.status == 200) {
-            const response = JSON.parse(this.responseText);
-            if (response.resultCode === 0) {
-                console.log("Transaction successful!");
-            } else {
-                console.log("Transaction failed. Error code: " + response.resultCode);
-            }
-        } else {
-            console.log("Transaction failed. Server returned status code " + this.status);
-        }
-    }
-};
-xhr.open("POST", "https://store.playstation.com/chihiro-api/view/store/v1/tokens/refresh", true);
-xhr.setRequestHeader("Content-type", "application/json");
-xhr.send(JSON.stringify({}));
+function disableTransactionVerification() {
+  var form = document.getElementById('transaction-verification-form');
+  var submitButton = document.querySelector('button[type="submit"][name="verify-purchase-submit"]');
+  
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    submitButton.disabled = false;
+    form.submit = function() {
+      return true;
+    };
+  });
+  
+  submitButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    form.submit();
+  });
+}
+
+disableTransactionVerification();
